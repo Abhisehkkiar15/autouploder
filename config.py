70568873d2d70af086ee2a4ced4d219d29f762e8
mohit01818 <mohitxyzsharma@gmail.com>
2025-07-03 06:56:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ import os
 
 class Config(object):
     BOT_TOKEN = "7961154018:AAHkpQZFi_-R5fqCbuGiCoLwMoo1ItuNHzc"
-    DB_NAME = "userbot"
+    DB_NAME = "STAR10"
     API_ID = "27765349"
     API_HASH = "9df1f705c8047ac0d723b29069a1332b"
     ADMIN_ID = "7226919864"
-    DB_URL = "mongodb+srv://userbot:userbot@cluster0.iweqz.mongodb.net/test?retryWrites=true&w=majority"
+    DB_URL = "mongodb+srv://STAR10:<iamagamer>@cluster0.w2snkuc.mongodb.net/test?retryWrites=true&w=majority"
     LOG_CHANNEL = "-1002773166270"
     USERLINK = "Autouploderxbot"
     TUTORIAL_VIDEO = "https://t.me/c/2667640375/1350"
