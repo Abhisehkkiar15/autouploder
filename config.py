f8707f7b26649f82b0b45c1fec2215153947ed04
mohit01818 <mohitxyzsharma@gmail.com>
2025-07-03 06:07:47 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+##Code Written By @ItsMeMaster
+##Code Written By @ItsMeMaster
+
+class Config(object):
+    BOT_TOKEN = "7743917201:AAGBjqqFVK7orEWj2bBO2g13-Ro0Qs_agQ4"
+    DB_NAME = "STAR10"
+    API_ID = "27765349"
+    API_HASH = "9df1f705c8047ac0d723b29069a1332b"
+    ADMIN_ID = "1116405290"
+    DB_URL = "mongodb+srv://STAR10:<iamagamer>@cluster0.w2snkuc.mongodb.net/"
+    LOG_CHANNEL = "https://t.me/+N5xSRah10TZlNmY1"
+    USERLINK = ""
+    TUTORIAL_VIDEO = ""
