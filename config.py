b3590a5822158ded4291e51d152fd4c175e51aa3
mohit01818 <mohitxyzsharma@gmail.com>
2025-07-03 06:58:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     API_ID = "27765349"
     API_HASH = "9df1f705c8047ac0d723b29069a1332b"
     ADMIN_ID = "7226919864"
-    DB_URL = "mongodb+srv://STAR10:<iamagamer>@cluster0.w2snkuc.mongodb.net/test?retryWrites=true&w=majority"
+    DB_URL = "mongodb+srv://STAR10:iamagamercluster0.w2snkuc.mongodb.net/test?retryWrites=true&w=majority"
     LOG_CHANNEL = "-1002773166270"
     USERLINK = "Autouploderxbot"
     TUTORIAL_VIDEO = "https://t.me/c/2667640375/1350"
