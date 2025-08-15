70c623fd32a0e4c791a0d0463690231138295c20
Abhisehkkiar15 <mpsystem05@gmail.com>
2025-08-15 18:50:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,11 @@ class Config(object):
     BOT_TOKEN = "8473234829:AAEKLZbZU-uRY7LQ7JzE600xe4APg0uSSvI"
     DB_NAME = "lund lelle"
     API_ID = "24037760"
-    API_HASH = "dccc3070f1c12ab155011f14c3d6ae6a""
+    API_HASH = "dccc3070f1c12ab155011f14c3d6ae6a"
     ADMIN_ID = "7290128282"
     DB_URL = "mongodb+srv://biklriplit:efaXfv2Ps9MRfner@cluster0.4hfu8zj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     LOG_CHANNEL = "-1002866717284"
     USERLINK = "https://t.me/sanam569"
     TUTORIAL_VIDEO = "https://t.me/c/2667640375/1350"
+
 
