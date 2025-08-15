8bbec0e68ac46185f1cb85a356bc28f94ea754f6
Abhisehkkiar15 <mpsystem05@gmail.com>
2025-08-15 18:42:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,13 @@ import os
 ##Code Written By @ItsMeMaster
 
 class Config(object):
-    BOT_TOKEN = "7961154018:AAHkpQZFi_-R5fqCbuGiCoLwMoo1ItuNHzc"
-    DB_NAME = "jaishreeram"
-    API_ID = "27765349"
-    API_HASH = "9df1f705c8047ac0d723b29069a1332b"
-    ADMIN_ID = "7226919864"
-    DB_URL = "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority"
-    LOG_CHANNEL = "-1002389882039"
-    USERLINK = "https://t.me/sahabji077"
+    BOT_TOKEN = "8473234829:AAEKLZbZU-uRY7LQ7JzE600xe4APg0uSSvI"
+    DB_NAME = "lund lelle"
+    API_ID = "24037760"
+    API_HASH = "dccc3070f1c12ab155011f14c3d6ae6a""
+    ADMIN_ID = "7290128282"
+    DB_URL = "mongodb+srv://biklriplit:efaXfv2Ps9MRfner@cluster0.4hfu8zj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    LOG_CHANNEL = "-1002866717284"
+    USERLINK = "https://t.me/sanam569"
     TUTORIAL_VIDEO = "https://t.me/c/2667640375/1350"
+
